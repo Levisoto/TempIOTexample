237f62fe72f741a9a574557756385b69c26adfcd
Levisoto <levisoto.337@gmail.com>
2018-10-25 14:49:03 -0500

    Connection with server Firebase succesfull

--- app/src/main/java/com/example/levelforall/leviss/tempiotexample/MainActivity.java
+++ app/src/main/java/com/example/levelforall/leviss/tempiotexample/MainActivity.java
@@ -3,11 +3,22 @@ package com.example.levelforall.leviss.tempiotexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase database;
+    DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("Kalu");
+
+        myRef.setValue("Never will be with Natu");
     }
 }

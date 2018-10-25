0d12948aaf122abe06594d4176cf64e0605ad32c
Levisoto <levisoto.337@gmail.com>
2018-10-25 17:37:03 -0500

    Android change simple data by a button

--- app/src/main/java/com/example/levelforall/leviss/tempiotexample/MainActivity.java
+++ app/src/main/java/com/example/levelforall/leviss/tempiotexample/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.levelforall.leviss.tempiotexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -11,14 +15,30 @@ public class MainActivity extends AppCompatActivity {
     FirebaseDatabase database;
     DatabaseReference myRef;
 
+    EditText editText;
+    TextView textView;
+    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editText = (EditText) findViewById(R.id.editText);
+        textView = (TextView) findViewById(R.id.textView);
+        button = (Button) findViewById(R.id.button);
+
+
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference("Kalu");
 
-        myRef.setValue("Never will be with Natu");
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myRef.setValue(editText.getText().toString());
+                textView.setText(editText.getText().toString());
+            }
+        });
     }
 }

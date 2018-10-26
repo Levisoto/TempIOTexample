ee404204d580cd9d774b4d752483ec6087f867f5
Levisoto <levisoto.337@gmail.com>
2018-10-25 19:10:56 -0500

    data from Firebase is showing on a listView

--- app/src/main/java/com/example/levelforall/leviss/tempiotexample/MainActivity.java
+++ app/src/main/java/com/example/levelforall/leviss/tempiotexample/MainActivity.java
@@ -1,44 +1,83 @@
 package com.example.levelforall.leviss.tempiotexample;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     FirebaseDatabase database;
     DatabaseReference myRef;
 
-    EditText editText;
-    TextView textView;
-    Button button;
+    ListView listView;
+    ArrayList<Integer> arrayList = new ArrayList<>();;
+    ArrayAdapter<Integer> arrayAdapter;
+
+    Context context;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = (EditText) findViewById(R.id.editText);
-        textView = (TextView) findViewById(R.id.textView);
-        button = (Button) findViewById(R.id.button);
+
+        listView = (ListView) findViewById(R.id.listview);
+        arrayAdapter = new ArrayAdapter<Integer>(MainActivity.this,android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
 
 
         database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("Kalu");
+        myRef = database.getReference("TempReport");
 
 
-        button.setOnClickListener(new View.OnClickListener() {
+        ValueEventListener postListener = new ValueEventListener() {
+
+
             @Override
-            public void onClick(View v) {
-                myRef.setValue(editText.getText().toString());
-                textView.setText(editText.getText().toString());
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                //Post post = dataSnapshot.getValue(Post.class);
+                //Log.d(TAG, " "+dataSnapshot.getValue());
+                arrayList.clear();
+
+                for(DataSnapshot postSnapshot : dataSnapshot.getChildren()){
+                    Integer value = postSnapshot.getValue(Integer.class);
+                    arrayList.add(value);
+                }
+
+                arrayAdapter.notifyDataSetChanged();
+
+                Log.d(TAG, " "+arrayList);
+
+
+                // ...
             }
-        });
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                // ...
+            }
+        };
+        myRef.addValueEventListener(postListener);
+
     }
 }

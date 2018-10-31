66e0b0ae970d435ab36ca871867f60868067d45e
Levisoto <levisoto.337@gmail.com>
2018-10-31 17:17:35 -0500

    App can plot data from Firebase in realtime but it has some bugs

--- app/src/main/java/com/example/levelforall/leviss/tempiotexample/MainActivity.java
+++ app/src/main/java/com/example/levelforall/leviss/tempiotexample/MainActivity.java
@@ -1,20 +1,28 @@
 package com.example.levelforall.leviss.tempiotexample;
 
+import android.graphics.Color;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.ListView;
+import android.widget.Switch;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
+import com.github.mikephil.charting.data.Entry;
 
 import java.util.ArrayList;
 
@@ -23,18 +31,18 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     FirebaseDatabase database;
     DatabaseReference myRef;
+    DatabaseReference myRef2;
 
     ListView listView;
     ArrayList<Integer> arrayList = new ArrayList<>();
     ArrayAdapter<Integer> arrayAdapter;
 
-    GraphView graph;
+    private LineChart mChart;
+    private Thread thread;
+    private boolean plotData = false;
 
-    private final Handler mHandler = new Handler();
-    private Runnable mTimer1;
-    private Runnable mTimer2;
-    private LineGraphSeries<DataPoint> mSeries1;
-    private double graph2LastXValue = 5d;
+
+    private Switch aSwitch;
 
 
     @Override
@@ -47,9 +55,23 @@ public class MainActivity extends AppCompatActivity {
         arrayAdapter = new ArrayAdapter<Integer>(MainActivity.this,android.R.layout.simple_list_item_1,arrayList);
         listView.setAdapter(arrayAdapter);
 
+        aSwitch = (Switch) findViewById(R.id.switch1);
+
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (aSwitch.isChecked()){
+                    plotData = true;
+                }else{
+                    plotData = false;
+                }
+            }
+        });
+
 
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference("TempReport");
+        myRef2 = database.getReference("ActualTemp");
 
 
         ValueEventListener postListener = new ValueEventListener() {
@@ -85,58 +107,166 @@ public class MainActivity extends AppCompatActivity {
         myRef.addValueEventListener(postListener);
 
 
-        graph = (GraphView) findViewById(R.id.graph);
-        initGraph(graph);
-
-        mSeries1 = new LineGraphSeries<>(generateFromArrayList(arrayList));
-        graph.addSeries(mSeries1);
+        ValueEventListener postListener2 = new ValueEventListener() {
 
 
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mTimer1 = new Runnable() {
             @Override
-            public void run() {
-                mSeries1.resetData(generateFromArrayList(arrayList));
-                mHandler.postDelayed(this, 100);
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                //Post post = dataSnapshot.getValue(Post.class);
+                //Log.d(TAG, " "+dataSnapshot.getValue());
+
+                Double real = (Double) dataSnapshot.child("realtime").getValue();
+
+                if (plotData){
+                    addEntry(real);
+                }
+
+                Log.d(TAG, "RealTime of Temp: "+real);
+
+
+                // ...
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                // ...
             }
         };
-        mHandler.postDelayed(mTimer1, 100);
+        myRef2.addValueEventListener(postListener2);
+
+
+        mChart = (LineChart) findViewById(R.id.chart1);
+        initGraph(mChart);
+        feedMultiple();
+
+
+
 
     }
 
-    private DataPoint[] generateFromArrayList(ArrayList<Integer> arrayList) {
-        DataPoint[] values = new DataPoint[arrayList.size()];
-        for(int i=0; i<arrayList.size();i++){
-            arrayList.get(i);
-            double x = i;
-            double y = arrayList.get(i);
-            DataPoint v = new DataPoint(x, y);
-            values[i] = v;
+    private LineDataSet createSet() {
+
+        LineDataSet set = new LineDataSet(null, "Dynamic Data");
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setLineWidth(3f);
+        set.setColor(Color.MAGENTA);
+        set.setHighlightEnabled(false);
+        set.setDrawValues(false);
+        set.setDrawCircles(false);
+        set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        set.setCubicIntensity(0.2f);
+        return set;
+    }
+
+    private void addEntry(Double event) {
+
+        LineData data = mChart.getData();
+
+        if (data != null) {
+
+            ILineDataSet set = data.getDataSetByIndex(0);
+            // set.addEntry(...); // can be called as well
+
+            if (set == null) {
+                set = createSet();
+                data.addDataSet(set);
+            }
+
+//            data.addEntry(new Entry(set.getEntryCount(), (float) (Math.random() * 80) + 10f), 0);
+            data.addEntry(new Entry(set.getEntryCount(), event.floatValue()), 0);
+            data.notifyDataChanged();
+
+            // let the chart know it's data has changed
+            mChart.notifyDataSetChanged();
+
+            // limit the number of visible entries
+            mChart.setVisibleXRangeMaximum(50);
+            // mChart.setVisibleYRange(30, AxisDependency.LEFT);
+
+            // move to the latest entry
+            mChart.moveViewToX(data.getEntryCount());
+
+        }
+    }
+
+    private void feedMultiple() {
+
+        if (thread != null){
+            thread.interrupt();
         }
 
-        return values;
+        thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                while (true){
+                    plotData = true;
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        thread.start();
     }
 
 
-    private void initGraph(GraphView graph) {
-        graph.getViewport().setYAxisBoundsManual(true);
-        graph.getViewport().setMinY(-200);
-        graph.getViewport().setMaxY(200);
+    private void initGraph(LineChart mChart) {
+        // enable description text
+        mChart.getDescription().setEnabled(true);
 
-        graph.getViewport().setXAxisBoundsManual(true);
-        graph.getViewport().setMinX(0);
-        graph.getViewport().setMaxX(50);
+        // enable touch gestures
+        mChart.setTouchEnabled(true);
 
-        graph.getGridLabelRenderer().setLabelVerticalWidth(15);
+        // enable scaling and dragging
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(true);
+        mChart.setDrawGridBackground(false);
 
-        // first mSeries is a line
-        mSeries1 = new LineGraphSeries<>();
-        mSeries1.setDrawDataPoints(true);
-        mSeries1.setDrawBackground(true);
-        graph.addSeries(mSeries1);
+        // if disabled, scaling can be done on x- and y-axis separately
+        mChart.setPinchZoom(true);
+
+        // set an alternative background color
+        mChart.setBackgroundColor(Color.WHITE);
+
+        LineData data = new LineData();
+        data.setValueTextColor(Color.WHITE);
+
+        // add empty data
+        mChart.setData(data);
+
+        // get the legend (only possible after setting data)
+        Legend l = mChart.getLegend();
+
+        // modify the legend ...
+        l.setForm(Legend.LegendForm.LINE);
+        l.setTextColor(Color.WHITE);
+
+        XAxis xl = mChart.getXAxis();
+        xl.setTextColor(Color.WHITE);
+        xl.setDrawGridLines(true);
+        xl.setAvoidFirstLastClipping(true);
+        xl.setEnabled(true);
+
+        YAxis leftAxis = mChart.getAxisLeft();
+        leftAxis.setTextColor(Color.WHITE);
+        leftAxis.setDrawGridLines(false);
+        leftAxis.setAxisMaximum(200f);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setDrawGridLines(true);
+
+        YAxis rightAxis = mChart.getAxisRight();
+        rightAxis.setEnabled(false);
+
+        mChart.getAxisLeft().setDrawGridLines(false);
+        mChart.getXAxis().setDrawGridLines(false);
+        mChart.setDrawBorders(false);
     }
 }
